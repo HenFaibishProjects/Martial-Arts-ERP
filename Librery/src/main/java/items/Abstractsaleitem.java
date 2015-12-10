ec5b25466f5d61a6229954ebb351b7ecf4004cfd
hen.f <hen.f@MaximM.solaredge.local>
2015-12-10 15:54:33 +0200

    devide to objects

---
+++ Librery/src/main/java/items/Abstractsaleitem.java
@@ -0,0 +1,47 @@
+package items;
+
+ 
+public abstract class Abstractsaleitem 
+{
+    String name; 
+	String barode;
+	int purchesPrice;
+
+	
+    public Abstractsaleitem( String name , String barode ,int purchesPrice ) {
+        this.name = name;
+        this.barode = barode;
+        this.purchesPrice = purchesPrice;
+      }
+                  
+	    public Abstractsaleitem( String barode) {
+	    	this.barode = barode;
+    }
+
+		public String getName() {
+			return name;
+		}
+
+		public void setName(String name) {
+			this.name = name;
+		}
+
+		public String getBarode() {
+			return barode;
+		}
+
+		public void setBarode(String barode) {
+			this.barode = barode;
+		}
+
+		public int getPurchesPrice() {
+			return purchesPrice;
+		}
+
+		public void setPurchesPrice(int purchesPrice) {
+			this.purchesPrice = purchesPrice;
+		}
+	    
+}
+
+
---
+++ Librery/src/main/java/offices/AbstractOffice.java
@@ -0,0 +1,50 @@
+package offices;
+
+ 
+public abstract class AbstractOffice 
+{
+    String name; 
+	String type;
+	String Address;
+
+	
+    public AbstractOffice( String name , String type ,String Address ) {
+        this.name = name;
+        this.type = type;
+        this.Address = Address;
+      
+    }
+
+
+	public String getName() {
+		return name;
+	}
+
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+
+	public String getType() {
+		return type;
+	}
+
+
+	public void setType(String type) {
+		this.type = type;
+	}
+
+
+	public String getAddress() {
+		return Address;
+	}
+
+
+	public void setAddress(String address) {
+		Address = address;
+	}
+
+}
+
+
